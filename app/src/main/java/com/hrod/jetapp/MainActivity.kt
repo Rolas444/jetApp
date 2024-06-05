d9cbfc0ed1041574e50ef5907937253d40454f21
DESKTOP-PR8MKLN\hrodland <7200ws@gmail.com>
2024-06-05 12:15:28 -0500

    primeras pruebas

--- app/src/main/java/com/hrod/jetapp/MainActivity.kt
+++ app/src/main/java/com/hrod/jetapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.hrod.jetapp.ui.theme.JetAppTheme
 
@@ -19,6 +21,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
+            Row{
+                Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "Una foto")
+            }
             Column {
                 Row {
                     nombreTarjeta("Rolas")
